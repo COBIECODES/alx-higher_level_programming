3ba91340505d694b31c97cafa0f1163f3bb0a9c1
COBIECODES <dibia3527@gmail.com>
2023-06-30 15:58:31 -0700

    task corrections

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+""" Defines the find_peak function """
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
-
-    if list_of_integers is None or list_of_integers == []:
+    """ Function for finding the peak element """
+    if not list_of_integers:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        # Check if mid element is a peak
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    # The peak element will be at index low
+    return list_of_integers[low]
