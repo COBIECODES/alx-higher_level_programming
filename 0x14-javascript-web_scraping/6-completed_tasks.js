deefda7437d6626ceebac15da6d104301d98e5b5
COBIECODES <dibia3527@gmail.com>
2023-07-25 23:39:35 +0000

    ask

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 
-const request = require('request');
+// Write a script that computes the number of tasks completed by user id.
 
+const request = require('request');
 const url = process.argv[2];
 
 request.get(url, { json: true }, (error, response, body) => {
   if (error) {
     console.log(error);
-i    return;
+    return;
   }
 
   const tasksCompleted = {};
@@ -20,3 +21,5 @@ i    return;
       }
     }
   });
+  console.log(tasksCompleted);
+});
